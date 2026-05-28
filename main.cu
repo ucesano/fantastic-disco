38edab39ebb557ab4772e46dab3ea09ba8545b0f
Ugo Cesano <ucesano@marvin.ucesano.com>
2026-05-28 23:41:14 +0200

    CUDA devices info now outputs in stderr.

--- main.cu
+++ main.cu
@@ -20,23 +20,23 @@ extern "C" {
 
 static void print_device_properties(cudaDeviceProp devProp)
 {
-    printf("Major revision number:         %d\n",  devProp.major);
-    printf("Minor revision number:         %d\n",  devProp.minor);
-    printf("Name:                          %s\n",  devProp.name);
-    printf("  Memory Clock rate:           %.0f Mhz\n", devProp.memoryClockRate * 1e-3f);
+    fprintf(stderr, "Major revision number:                           %d\n",  devProp.major);
+    fprintf(stderr, "Minor revision number:                           %d\n",  devProp.minor);
+    fprintf(stderr, "Name:                                            %s\n",  devProp.name);
+    fprintf(stderr, "  Memory Clock rate:                             %.0f Mhz\n", devProp.memoryClockRate * 1e-3f);
 
-    printf("  Memory Bus Width:            %d bit\n",devProp.memoryBusWidth);
+    fprintf(stderr, "  Memory Bus Width:                              %d bit\n",devProp.memoryBusWidth);
 
-    printf("  Peak Memory Bandwidth:       %7.3f GB/s\n",2.0*devProp.memoryClockRate*(devProp.memoryBusWidth/8)/1.0e6);
+    fprintf(stderr, "  Peak Memory Bandwidth:                         %7.3f GB/s\n",2.0*devProp.memoryClockRate*(devProp.memoryBusWidth/8)/1.0e6);
 
-    printf("  Multiprocessors:             %3d\n",devProp.multiProcessorCount);
-    printf("  Maximum number of threads per multiprocessor:  %d\n",devProp.maxThreadsPerMultiProcessor);
-    printf("  Maximum number of threads per block:           %d\n",devProp.maxThreadsPerBlock);
-    printf("  Max dimension size of a thread block (x,y,z): (%d, %d, %d)\n",
+    fprintf(stderr, "  Multiprocessors:                               %3d\n",devProp.multiProcessorCount);
+    fprintf(stderr, "  Maximum number of threads per multiprocessor:  %d\n",devProp.maxThreadsPerMultiProcessor);
+    fprintf(stderr, "  Maximum number of threads per block:           %d\n",devProp.maxThreadsPerBlock);
+    fprintf(stderr, "  Max dimension size of a thread block (x,y,z): (%d, %d, %d)\n",
            devProp.maxThreadsDim[0], devProp.maxThreadsDim[1],devProp.maxThreadsDim[2]);
-    printf("  Max dimension size of a grid size    (x,y,z): (%d, %d, %d)\n",
+    fprintf(stderr, "  Max dimension size of a grid size    (x,y,z): (%d, %d, %d)\n",
            devProp.maxGridSize[0], devProp.maxGridSize[1],devProp.maxGridSize[2]);
-    printf("  Total amount of shared memory per block:       %zu bytes\n", devProp.sharedMemPerBlock);
+    fprintf(stderr, "  Total amount of shared memory per block:       %zu bytes\n", devProp.sharedMemPerBlock);
     putchar(10);
 }
 
@@ -44,12 +44,12 @@ static void display_card_informations(void)
 {
     int devCount;
     cudaGetDeviceCount(&devCount);
-    printf("CUDA Device Query...\n");
-    printf("There are %d CUDA devices.\n", devCount);
+    fprintf(stderr, "CUDA Device Query...\n");
+    fprintf(stderr, "There are %d CUDA devices.\n", devCount);
 
     for (int i = 0; i < devCount; ++i)
     {
-        printf("\nCUDA Device #%d\n", i);
+        fprintf(stderr, "\nCUDA Device #%d\n", i);
         cudaDeviceProp devProp;
         cudaGetDeviceProperties(&devProp, i);
         print_device_properties(devProp);
