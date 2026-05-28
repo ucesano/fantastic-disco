c326a2a3a5576b72691ada1a4a7ae10ffe17acc0
ucesano <ucesano@etik.com>
2026-05-28 23:49:04 +0200

    Executable can now take n mtx files as args.

--- main.cu
+++ main.cu
@@ -85,110 +85,111 @@ int main(int argc, char ** argv)
         fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    else
+
+    for (int fi = 1; fi < argc; ++fi)
     {
-        if ((f = fopen(argv[1], "r")) == NULL)
+        if ((f = fopen(argv[fi], "r")) == NULL)
             exit(EXIT_FAILURE);
-    }
 
-    if (mm_read_banner(f, &matcode) != 0)
-    {
-        printf("Could not process Matrix Market banner.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (mm_is_complex(matcode) && mm_is_matrix(matcode) && mm_is_sparse(matcode))
-    {
-        printf("Sorry, this application does not support ");
-        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
-        exit(1);
-    }
-
-    /* finding out size of sparse matrix. */
-    if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &nz)) != 0)
-        exit(EXIT_FAILURE);
-
-    /* reserving memory for COO representation. */
-    I = (int *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(int));
-    J = (int *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(int));
-    val = (float *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(float));
-
-    if (mm_is_general(matcode))
-    {
-        for(i = 0; i < nz; ++i)
+        if (mm_read_banner(f, &matcode) != 0)
         {
-            fscanf(f, "%d %d %g\n", &I[i], &J[i], &val[i]);
-            I[i]--;
-            J[i]--;
+            printf("Could not process Matrix Market banner.\n");
+            exit(EXIT_FAILURE);
         }
-    }
-    else if (mm_is_symmetric(matcode))
-    {
-        int extra = 0;
 
-        for(i = 0; i < nz; ++i)
+        if (mm_is_complex(matcode) && mm_is_matrix(matcode) && mm_is_sparse(matcode))
         {
-            fscanf(f, "%d %d %g\n", &I[i], &J[i], &val[i]);
-            I[i]--;
-            J[i]--;
+            printf("Sorry, this application does not support ");
+            printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
+            exit(1);
+        }
 
-            if (I[i] != J[i])
+        /* finding out size of sparse matrix. */
+        if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &nz)) != 0)
+            exit(EXIT_FAILURE);
+
+        /* reserving memory for COO representation. */
+        I = (int *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(int));
+        J = (int *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(int));
+        val = (float *) malloc(nz * (1 + mm_is_symmetric(matcode)) * sizeof(float));
+
+        if (mm_is_general(matcode))
+        {
+            for(i = 0; i < nz; ++i)
             {
-                I[nz + extra] = J[i];
-                J[nz + extra] = I[i];
-                val[nz + extra] = val[i];
+                fscanf(f, "%d %d %g\n", &I[i], &J[i], &val[i]);
+                I[i]--;
+                J[i]--;
+            }
+        }
+        else if (mm_is_symmetric(matcode))
+        {
+            int extra = 0;
 
-                extra++;
+            for(i = 0; i < nz; ++i)
+            {
+                fscanf(f, "%d %d %g\n", &I[i], &J[i], &val[i]);
+                I[i]--;
+                J[i]--;
+
+                if (I[i] != J[i])
+                {
+                    I[nz + extra] = J[i];
+                    J[nz + extra] = I[i];
+                    val[nz + extra] = val[i];
+
+                    extra++;
+                }
+            }
+
+            nz += extra;
+
+            int *tI, *tJ;
+            float *tval;
+
+            tI = (int *)realloc(I, nz * sizeof(int));
+            tJ = (int *)realloc(J, nz * sizeof(int));
+            tval = (float *)realloc(val, nz * sizeof(float));
+
+            if (tI != NULL && tJ != NULL && tval != NULL)
+            {
+                I = tI;
+                J = tJ;
+                val = tval;
             }
         }
 
-        nz += extra;
+        fclose(f);
 
-        int *tI, *tJ;
-        float *tval;
+        mm_sort_coo(I, J, val, nz);
 
-        tI = (int *)realloc(I, nz * sizeof(int));
-        tJ = (int *)realloc(J, nz * sizeof(int));
-        tval = (float *)realloc(val, nz * sizeof(float));
+        O = (int *)calloc((M + 1), sizeof(int));
+        mm_coo_to_csr_row_ptr(I, nz, M, O);
 
-        if (tI != NULL && tJ != NULL && tval != NULL)
-        {
-            I = tI;
-            J = tJ;
-            val = tval;
-        }
+        row_nz = (int *)calloc(M, sizeof(int));
+        for (i = 0; i < nz; ++i) row_nz[I[i]]++;
+
+        X = (float *)malloc(N * sizeof(float));
+        for (i = 0; i < N; i++) X[i] = 100.f * (float)genrand_real1() + 1.f;
+
+        struct results coo     = spmv_gpu_coo_prof(I, J, val, M, N, nz, row_nz, X);
+        struct results csr     = spmv_gpu_csr_prof(O, J, val, M, N, nz, row_nz, X);
+        struct results csr_opt = spmv_gpu_csr_opt_prof(O, J, val, M, N, nz, row_nz, X);
+
+        free(row_nz);
+        free(X);
+        free(O);
+        free(I);
+        free(J);
+        free(val);
+
+        display_card_informations();
+
+        fprintf(stdout, "file: %s\n", argv[fi]);
+        print_results(coo, "coo");
+        print_results(csr, "csr");
+        print_results(csr_opt, "csr (warp)");
     }
 
-    fclose(f);
-
-    mm_sort_coo(I, J, val, nz);
-
-    O = (int *)calloc((M + 1), sizeof(int));
-    mm_coo_to_csr_row_ptr(I, nz, M, O);
-
-    row_nz = (int *)calloc(M, sizeof(int));
-    for (i = 0; i < nz; ++i) row_nz[I[i]]++;
-
-    X = (float *)malloc(N * sizeof(float));
-    for (i = 0; i < N; i++) X[i] = 100.f * (float)genrand_real1() + 1.f;
-
-    struct results coo     = spmv_gpu_coo_prof(I, J, val, M, N, nz, row_nz, X);
-    struct results csr     = spmv_gpu_csr_prof(O, J, val, M, N, nz, row_nz, X);
-    struct results csr_opt = spmv_gpu_csr_opt_prof(O, J, val, M, N, nz, row_nz, X);
-
-    free(row_nz);
-    free(X);
-    free(O);
-    free(I);
-    free(J);
-    free(val);
-
-    display_card_informations();
-
-    fprintf(stdout, "file: %s\n", argv[1]);
-    print_results(coo, "coo");
-    print_results(csr, "csr");
-    print_results(csr_opt, "csr (warp)");
-
-    return ret_code;
+    return EXIT_SUCCESS;
 }
